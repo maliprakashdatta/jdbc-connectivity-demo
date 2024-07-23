3616fcc369eaefeee44419a703d8b4e3445fa550
Mali Prakash Datta <152262211+maliprakashdatta@users.noreply.github.com>
2024-07-23 17:17:44 +0530

    Update StudentandAddress.sql

--- StudentandAddress.sql
+++ StudentandAddress.sql
@@ -1,1 +1,6 @@
-
+-----------------------------------------------
+CREATE TABLE address (
+  address_id INT NOT NULL,
+  city VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
+--------------------------------------------------
