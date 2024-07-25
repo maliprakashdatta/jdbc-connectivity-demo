7e187c86107cc96389485780c35b0d8b7827fefd
Mali Prakash Datta <152262211+maliprakashdatta@users.noreply.github.com>
2024-07-25 23:49:27 +0530

    Update StudentandAddress.sql

--- StudentandAddress.sql
+++ StudentandAddress.sql
@@ -52,7 +52,7 @@ SELECT * FROM student;
 INSERT INTO student (student_Id, name, address, percentage, grade) VALUES ('1', 'Prakash', 'Jath', '78.4', 'A');
 INSERT INTO student(student_Id, name, address, percentage, grade) VALUES ('2', 'Nirav', 'Pune', '80.45', 'A');
 
-UPDATE student SET percentage = '36.66' WHERE student_id = 1;
+UPDATE student SET percentage = '79.66' WHERE student_id = 1;
 
 
 select * from student;
@@ -60,67 +60,5 @@ select * from student;
 ALTER TABLE student
 DROP COLUMN percentage;
 
-DROP TABLE student;
--- Pls insert data again ---------------------------------------------
+SELECT * FROM student;
 
-select student_id, name, address_id, percentage from student;
-select max(percentage) from student;
-select min(percentage) from student;
-select avg(percentage) from student;
-
-select * from student where percentage = (select max(percentage) from student);
-
-select * from student order by percentage DESC limit 3;
-
-select count(*) from student where percentage > '60.00' and percentage < '80.00';
-ALTER TABLE student
-ADD COLUMN grade VARCHAR(128);
-
-select * from student;
-
-UPDATE student SET grade = 'Distinction' WHERE percentage > '75.00';
-UPDATE student SET grade = 'First Class' WHERE percentage > '65.00' and percentage < '75.00';
-UPDATE student SET grade = 'Second Class' WHERE percentage > '55.00' and percentage < '65.00';
-UPDATE student SET grade = 'Well Pass' WHERE percentage > '45.00' and percentage < '55.00';
-UPDATE student SET grade = 'Fail' WHERE percentage < '45.00';
-
-select distinct(grade) from student;
-
-select student_id, name, grade, percentage, rank() OVER (order by percentage desc) from student;
-
-select student_id, name, grade, percentage, rank() OVER (order by percentage desc) from student LIMIT 10 OFFSET 2;
-
-SELECT
-	s.student_id,
-	s.name,
-	s.percentage,
-	s.grade,
-	a.city,
-	rank() OVER (order by s.percentage desc)
-FROM student s JOIN address a
-ON a.address_id = s.address_id
-WHERE s.percentage > '80.00'
-ORDER BY s.percentage desc LIMIT 5;
-
-SELECT
-	a.city
-FROM address a LEFT JOIN Student s ON a.address_id = s.address_id;
-
-SELECT a.city
-FROM address a RIGHT JOIN Student s ON a.address_id = s.address_id;
-
-
-DELETE FROM STUDENT;
-DELETE FROM ADDRESS;
-
-INSERT INTO address (address_id, city) VALUES
-(1, 'Pune, Shivajinager'),
-(2, 'Pune, Hadapsar'),
-(3, 'Kolhapur'),
-(4, 'Nashik'),
-(5, 'Solapur'),
-(6, 'Thane'),
-(7, 'Mumbai'),
-(8, 'Nagpur'),
-(9, 'Satara'),
-(10, 'Sangli');
