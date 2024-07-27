10112f4bceb599877415a031bf1a67f7ad8b2a5b
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-07-27 19:14:15 +0530

    Address Services program

--- src/main/java/org/example/service/AddressServices.java
+++ src/main/java/org/example/service/AddressServices.java
@@ -1,47 +1,15 @@
-
-
 package org.example.service;
-import java.util.Scanner;
+
 import org.example.model.Address;
-import org.example.model.Student;
 import org.example.repository.AddressRepository;
-import java.sql.SQLException;
+
 import java.util.List;
+
 public class AddressServices {
 
-    private static final AddressRepository STUDENT_REPO = new AddressRepository();
+    private static final AddressRepository ADDRESS_REPO = new AddressRepository();
 
     public List<Address> retrieveAddresses() {
-        return STUDENT_REPO.retrieveAddresses();
-    }
-
-    public void insertStudent() {
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Pls enter address id:");
-        int studentId = Integer.parseInt(scanner.nextLine());
-        System.out.println("Pls enter city :");
-        String name = scanner.nextLine();
-
-        System.out.println("Pls enter student Address Id:");
-        int addressId = Integer.parseInt(scanner.nextLine());
-        System.out.println("Pls enter student percentage:");
-        float percentage = Float.parseFloat(scanner.nextLine());
-        System.out.println("Pls enter student grade:");
-        String grade = scanner.nextLine();
-
-        Address address = STUDENT_REPO.retrieveAddress(addressId);
-        Address student = new Address(addressId, address.getCity());
-
-        try {
-            if (STUDENT_REPO.insertStudent(student)) {
-                System.out.println("Employee inserted successfully!");
-            } else {
-                System.out.println("Failed to insert employee.");
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        return ADDRESS_REPO.retrieveAddresses();
     }
 }
-
