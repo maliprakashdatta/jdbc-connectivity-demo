1ff9e4726100072d5198c07484e6d6911eb5b700
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-07-31 19:56:55 +0530

    Connection Services..

--- src/main/java/com/swsa/service/ConnectionService.java
+++ src/main/java/com/swsa/service/ConnectionService.java
@@ -1,20 +1,23 @@
-package org.example.service;
+package com.swsa.service;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
 public class ConnectionService
 {
-
     public Connection getConnection() {
         Connection connection = null;
         try {
-            String url = "jdbc:mysql://localhost:3306/student";
+
+            String url = "jdbc:mysql://localhost:3306/ssbms";
             String username = "root";
             String password = "Prakash@123";
             connection = DriverManager.getConnection(url, username, password);
             System.out.println("Connected to the database!");
-        } catch (SQLException e) {
-            System.err.println("Connection error: " + e.getMessage());
+        }
+        catch (SQLException e)
+        {
+            System.err.println("Connection error: "+e.getMessage());
         }
         return connection;
     }
